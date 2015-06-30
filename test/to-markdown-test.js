ab40773e9ef66f70549606b839674063268970cf
Glenn Gentzke <glenn.gentzke@gmail.com>
2015-06-30 15:54:19 -0400

    Updates toMarkdown to print empty string when no text is given for an anchor since there is no markdown output.  Auto-jsbeautified code.

--- test/to-markdown-test.js
+++ test/to-markdown-test.js
@@ -76,7 +76,8 @@ test('anchors', function() {
     ['<a href="http://example.com/about">About us</a>', '[About us](http://example.com/about)', 'a'],
     ['<a href="http://example.com/about" title="About this company">About us</a>', '[About us](http://example.com/about "About this company")', 'a with title'],
     ['<a id="donuts3">About us</a>', '<a id="donuts3">About us</a>', 'a with no src'],
-    ['<a href="http://example.com/about"><span>About us</span></a>', '[<span>About us</span>](http://example.com/about)', 'with a span']
+    ['<a href="http://example.com/about"><span>About us</span></a>', '[<span>About us</span>](http://example.com/about)', 'with a span'],
+    ['<a href="http://example.com/about"> </a>', '', 'a without text']
   ]);
 });
 
@@ -84,37 +85,41 @@ test('pre/code blocks', function() {
   runTestCases([
     [
       ['<pre><code>def hello_world',
-      '  # 42 &lt; 9001',
-      '  "Hello world!"',
-      'end</code></pre>'].join('\n'),
+        '  # 42 &lt; 9001',
+        '  "Hello world!"',
+        'end</code></pre>'
+      ].join('\n'),
 
       ['    def hello_world',
-      '      # 42 < 9001',
-      '      "Hello world!"',
-      '    end'].join('\n')
+        '      # 42 < 9001',
+        '      "Hello world!"',
+        '    end'
+      ].join('\n')
     ],
     [
       ['<pre><code>def foo',
-      '  # 42 &lt; 9001',
-      '  \'Hello world!\'',
-      'end</code></pre>',
-      '<p>next:</p>',
-      '<pre><code>def bar',
-      '  # 42 &lt; 9001',
-      '  \'Hello world!\'',
-      'end</code></pre>'].join('\n'),
+        '  # 42 &lt; 9001',
+        '  \'Hello world!\'',
+        'end</code></pre>',
+        '<p>next:</p>',
+        '<pre><code>def bar',
+        '  # 42 &lt; 9001',
+        '  \'Hello world!\'',
+        'end</code></pre>'
+      ].join('\n'),
 
       ['    def foo',
-      '      # 42 < 9001',
-      '      \'Hello world!\'',
-      '    end',
-      '',
-      'next:',
-      '',
-      '    def bar',
-      '      # 42 < 9001',
-      '      \'Hello world!\'',
-      '    end'].join('\n'),
+        '      # 42 < 9001',
+        '      \'Hello world!\'',
+        '    end',
+        '',
+        'next:',
+        '',
+        '    def bar',
+        '      # 42 < 9001',
+        '      \'Hello world!\'',
+        '    end'
+      ].join('\n'),
 
       'Multiple pre/code blocks'
     ],
@@ -129,19 +134,21 @@ test('lists', function() {
     ['<ul>\n\t<li>Hello world</li>\n\t<li>Foo bar</li>\n</ul>', '*   Hello world\n*   Foo bar', 'ul'],
     [
       ['<ul>',
-      '  <li>Hello world</li>',
-      '  <li>Lorem ipsum</li>',
-      '</ul>',
-      '<ul>',
-      '  <li>Hello world</li>',
-      '  <li>Lorem ipsum</li>',
-      '</ul>'].join('\n'),
+        '  <li>Hello world</li>',
+        '  <li>Lorem ipsum</li>',
+        '</ul>',
+        '<ul>',
+        '  <li>Hello world</li>',
+        '  <li>Lorem ipsum</li>',
+        '</ul>'
+      ].join('\n'),
 
       ['*   Hello world',
-      '*   Lorem ipsum',
-      '',
-      '*   Hello world',
-      '*   Lorem ipsum'].join('\n'),
+        '*   Lorem ipsum',
+        '',
+        '*   Hello world',
+        '*   Lorem ipsum'
+      ].join('\n'),
 
       'Multiple uls'
     ],
@@ -152,98 +159,106 @@ test('lists', function() {
     ],
     [
       ['<ol>',
-      '  <li>',
-      '    <p>This is a list item with two paragraphs.</p>',
-      '    <p>Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus.</p>',
-      '  </li>',
-      '  <li>',
-      '    <p>Suspendisse id sem consectetuer libero luctus adipiscing.</p>',
-      '  </li>',
-      '</ol>'].join('\n'),
+        '  <li>',
+        '    <p>This is a list item with two paragraphs.</p>',
+        '    <p>Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus.</p>',
+        '  </li>',
+        '  <li>',
+        '    <p>Suspendisse id sem consectetuer libero luctus adipiscing.</p>',
+        '  </li>',
+        '</ol>'
+      ].join('\n'),
 
       ['1.  This is a list item with two paragraphs.',
-      '',
-      '    Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus.',
-      '',
-      '2.  Suspendisse id sem consectetuer libero luctus adipiscing.'].join('\n'),
+        '',
+        '    Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus.',
+        '',
+        '2.  Suspendisse id sem consectetuer libero luctus adipiscing.'
+      ].join('\n'),
 
       'ol with multiple ps'
     ],
     [
       ['<ul>',
-      '  <li>This is a list item at root level</li>',
-      '  <li>This is another item at root level</li>',
-      '  <li>',
-      '    <ul>',
-      '      <li>This is a nested list item</li>',
-      '      <li>This is another nested list item</li>',
-      '      <li>',
-      '        <ul>',
-      '          <li>This is a deeply nested list item</li>',
-      '          <li>This is another deeply nested list item</li>',
-      '          <li>This is a third deeply nested list item</li>',
-      '        </ul>',
-      '      </li>',
-      '    </ul>',
-      '  </li>',
-      '  <li>This is a third item at root level</li>',
-      '</ul>'].join('\n'),
+        '  <li>This is a list item at root level</li>',
+        '  <li>This is another item at root level</li>',
+        '  <li>',
+        '    <ul>',
+        '      <li>This is a nested list item</li>',
+        '      <li>This is another nested list item</li>',
+        '      <li>',
+        '        <ul>',
+        '          <li>This is a deeply nested list item</li>',
+        '          <li>This is another deeply nested list item</li>',
+        '          <li>This is a third deeply nested list item</li>',
+        '        </ul>',
+        '      </li>',
+        '    </ul>',
+        '  </li>',
+        '  <li>This is a third item at root level</li>',
+        '</ul>'
+      ].join('\n'),
 
       ['*   This is a list item at root level',
-      '*   This is another item at root level',
-      '*   *   This is a nested list item',
-      '    *   This is another nested list item',
-      '    *   *   This is a deeply nested list item',
-      '        *   This is another deeply nested list item',
-      '        *   This is a third deeply nested list item',
-      '*   This is a third item at root level'].join('\n'),
+        '*   This is another item at root level',
+        '*   *   This is a nested list item',
+        '    *   This is another nested list item',
+        '    *   *   This is a deeply nested list item',
+        '        *   This is another deeply nested list item',
+        '        *   This is a third deeply nested list item',
+        '*   This is a third item at root level'
+      ].join('\n'),
 
       'Nested uls'
     ],
     [
       ['<ul>',
-      '  <li>This is a list item at root level</li>',
-      '  <li>This is another item at root level</li>',
-      '  <li>',
-      '    <ol>',
-      '      <li>This is a nested list item</li>',
-      '      <li>This is another nested list item</li>',
-      '      <li>',
-      '        <ul>',
-      '          <li>This is a deeply nested list item</li>',
-      '          <li>This is another deeply nested list item</li>',
-      '          <li>This is a third deeply nested list item</li>',
-      '        </ul>',
-      '      </li>',
-      '    </ol>',
-      '  </li>',
-      '  <li>This is a third item at root level</li>',
-      '</ul>'].join('\n'),
+        '  <li>This is a list item at root level</li>',
+        '  <li>This is another item at root level</li>',
+        '  <li>',
+        '    <ol>',
+        '      <li>This is a nested list item</li>',
+        '      <li>This is another nested list item</li>',
+        '      <li>',
+        '        <ul>',
+        '          <li>This is a deeply nested list item</li>',
+        '          <li>This is another deeply nested list item</li>',
+        '          <li>This is a third deeply nested list item</li>',
+        '        </ul>',
+        '      </li>',
+        '    </ol>',
+        '  </li>',
+        '  <li>This is a third item at root level</li>',
+        '</ul>'
+      ].join('\n'),
 
       ['*   This is a list item at root level',
-      '*   This is another item at root level',
-      '*   1.  This is a nested list item',
-      '    2.  This is another nested list item',
-      '    3.  *   This is a deeply nested list item',
-      '        *   This is another deeply nested list item',
-      '        *   This is a third deeply nested list item',
-      '*   This is a third item at root level'].join('\n'),
+        '*   This is another item at root level',
+        '*   1.  This is a nested list item',
+        '    2.  This is another nested list item',
+        '    3.  *   This is a deeply nested list item',
+        '        *   This is another deeply nested list item',
+        '        *   This is a third deeply nested list item',
+        '*   This is a third item at root level'
+      ].join('\n'),
 
       'Nested ols'
     ],
     [
       ['<ul>',
-      '  <li>',
-      '    <p>A list item with a blockquote:</p>',
-      '    <blockquote>',
-      '      <p>This is a blockquote inside a list item.</p>',
-      '    </blockquote>',
-      '  </li>',
-      '</ul>'].join('\n'),
+        '  <li>',
+        '    <p>A list item with a blockquote:</p>',
+        '    <blockquote>',
+        '      <p>This is a blockquote inside a list item.</p>',
+        '    </blockquote>',
+        '  </li>',
+        '</ul>'
+      ].join('\n'),
 
       ['*   A list item with a blockquote:',
-      '',
-      '    > This is a blockquote inside a list item.'].join('\n'),
+        '',
+        '    > This is a blockquote inside a list item.'
+      ].join('\n'),
 
       'ul with blockquote'
     ]
@@ -254,69 +269,75 @@ test('blockquotes', function() {
   runTestCases([
     [
       ['<blockquote>',
-      '  <p>This is a blockquote with two paragraphs.</p>',
-      '',
-      '  <p>Donec sit amet nisl.</p>',
-      '</blockquote>'].join('\n'),
+        '  <p>This is a blockquote with two paragraphs.</p>',
+        '',
+        '  <p>Donec sit amet nisl.</p>',
+        '</blockquote>'
+      ].join('\n'),
 
       ['> This is a blockquote with two paragraphs.',
-      '> ',
-      '> Donec sit amet nisl.'].join('\n'),
+        '> ',
+        '> Donec sit amet nisl.'
+      ].join('\n'),
 
       'blockquote with two ps'
     ],
     [
       ['<blockquote>',
-      '  <p>This is the first level of quoting.</p>',
-      '',
-      '  <blockquote>',
-      '    <p>This is nested blockquote.</p>',
-      '  </blockquote>',
-      '',
-      '  <p>Back to the first level.</p>',
-      '</blockquote>'].join('\n'),
+        '  <p>This is the first level of quoting.</p>',
+        '',
+        '  <blockquote>',
+        '    <p>This is nested blockquote.</p>',
+        '  </blockquote>',
+        '',
+        '  <p>Back to the first level.</p>',
+        '</blockquote>'
+      ].join('\n'),
 
       ['> This is the first level of quoting.',
-      '> ',
-      '> > This is nested blockquote.',
-      '> ',
-      '> Back to the first level.'].join('\n'),
+        '> ',
+        '> > This is nested blockquote.',
+        '> ',
+        '> Back to the first level.'
+      ].join('\n'),
 
       'Nested blockquotes'
     ],
     [
       ['<blockquote>',
-      '  <h2>This is a header.</h2>',
-      '  <ol>',
-      '    <li>This is the first list item.</li>',
-      '    <li>This is the second list item.</li>',
-      '  </ol>',
-      '  <p>Here\'s some example code:</p>',
-      '  <pre><code>return 1 &lt; 2 ? shell_exec(\'echo $input | $markdown_script\') : 0;</code></pre>',
-      '</blockquote>'].join('\n'),
+        '  <h2>This is a header.</h2>',
+        '  <ol>',
+        '    <li>This is the first list item.</li>',
+        '    <li>This is the second list item.</li>',
+        '  </ol>',
+        '  <p>Here\'s some example code:</p>',
+        '  <pre><code>return 1 &lt; 2 ? shell_exec(\'echo $input | $markdown_script\') : 0;</code></pre>',
+        '</blockquote>'
+      ].join('\n'),
 
       ['> ## This is a header.',
-      '> ',
-      '> 1.  This is the first list item.',
-      '> 2.  This is the second list item.',
-      '> ',
-      '> Here\'s some example code:',
-      '> ',
-      '>     return 1 < 2 ? shell_exec(\'echo $input | $markdown_script\') : 0;'].join('\n'),
+        '> ',
+        '> 1.  This is the first list item.',
+        '> 2.  This is the second list item.',
+        '> ',
+        '> Here\'s some example code:',
+        '> ',
+        '>     return 1 < 2 ? shell_exec(\'echo $input | $markdown_script\') : 0;'
+      ].join('\n'),
 
       'html in blockquote'
     ]
   ]);
 });
 
-test('block-level', function () {
+test('block-level', function() {
   runTestCases([
     ['<div>Hello</div><div>world</div>', '<div>Hello</div>\n\n<div>world</div>', 'divs separated by \\n\\n'],
     ['<div><em>hello</em></div>', '<div>_hello_</div>']
   ]);
 });
 
-test('comments', function () {
+test('comments', function() {
   equal(toMarkdown('<!-- comment -->'), '', 'comments removed');
 });
 
@@ -330,46 +351,50 @@ test('leading/trailing whitespace', function() {
     ['<h1>\n    Header text', '# Header text', 'Leading whitespace in h1'],
     [
       ['<ol>',
-      '  <li>Chapter One',
-      '    <ol>',
-      '      <li>Section One</li>',
-      '      <li>Section Two </li>',
-      '      <li>Section Three </li>',
-      '    </ol>',
-      '  </li>',
-      '  <li>Chapter Two</li>',
-      '  <li>Chapter Three  </li>',
-      '</ol>'].join('\n'),
+        '  <li>Chapter One',
+        '    <ol>',
+        '      <li>Section One</li>',
+        '      <li>Section Two </li>',
+        '      <li>Section Three </li>',
+        '    </ol>',
+        '  </li>',
+        '  <li>Chapter Two</li>',
+        '  <li>Chapter Three  </li>',
+        '</ol>'
+      ].join('\n'),
 
       ['1.  Chapter One',
-      '    1.  Section One',
-      '    2.  Section Two',
-      '    3.  Section Three',
-      '2.  Chapter Two',
-      '3.  Chapter Three'].join('\n'),
+        '    1.  Section One',
+        '    2.  Section Two',
+        '    3.  Section Three',
+        '2.  Chapter Two',
+        '3.  Chapter Three'
+      ].join('\n'),
 
       'Trailing whitespace in li'
     ],
     [
       ['<ul>',
-      '  <li>', // Multilined
-      '    Foo ',
-      '  </li>',
-      '  <li>', // Bizarre formatting
-      '    <strong>Bar </strong> </li>',
-      '  <li>Baz</li>',
-      '</ul>',
-      '<ol>',
-      '  <li> Hello',
-      '                      world',
-      '  </li>',
-      '</ol>'].join('\n'),
+        '  <li>', // Multilined
+        '    Foo ',
+        '  </li>',
+        '  <li>', // Bizarre formatting
+        '    <strong>Bar </strong> </li>',
+        '  <li>Baz</li>',
+        '</ul>',
+        '<ol>',
+        '  <li> Hello',
+        '                      world',
+        '  </li>',
+        '</ol>'
+      ].join('\n'),
 
       ['*   Foo',
-      '*   **Bar**',
-      '*   Baz',
-      '',
-      '1.  Hello world'].join('\n')
+        '*   **Bar**',
+        '*   Baz',
+        '',
+        '1.  Hello world'
+      ].join('\n')
     ],
     [
       'Hello world.<em> Foo </em><strong>bar </strong>',
@@ -379,18 +404,18 @@ test('leading/trailing whitespace', function() {
   ]);
 });
 
-test('blank', function () {
+test('blank', function() {
   runTestCases([
     ['<div></div>', '', 'Blank div'],
     ['<em></em>', '', 'Blank em'],
     ['<strong><br></strong>', '', 'Blank strong with br'],
-    ['<a href="#foo"></a>', '[](#foo)', 'Blank a'],
+    ['<a href="#foo"></a>', '', 'Blank a'],
   ]);
 });
 
 test('custom converters', function() {
   var html, converter, md = '*Hello world*';
-  var replacement = function (innerHTML) {
+  var replacement = function(innerHTML) {
     return '*' + innerHTML + '*';
   };
 
@@ -399,35 +424,47 @@ test('custom converters', function() {
     filter: 'span',
     replacement: replacement
   };
-  equal(toMarkdown(html, {converters: [converter]}), md, 'Custom filter string');
+  equal(toMarkdown(html, {
+    converters: [converter]
+  }), md, 'Custom filter string');
 
   html = '<span>Hello world</span>';
   converter = {
     filter: ['span'],
     replacement: replacement
   };
-  equal(toMarkdown(html, {converters: [converter]}), md, 'Custom filter array');
+  equal(toMarkdown(html, {
+    converters: [converter]
+  }), md, 'Custom filter array');
 
   html = '<span style="font-style: italic">Hello world</span>';
   converter = {
-    filter: function (node) {
+    filter: function(node) {
       return node.tagName === 'SPAN' && /italic/i.test(node.style.fontStyle);
     },
     replacement: replacement
   };
-  equal(toMarkdown(html, {converters: [converter]}), md, 'Custom filter function');
+  equal(toMarkdown(html, {
+    converters: [converter]
+  }), md, 'Custom filter function');
 });
 
-test('invalid input', function () {
-  throws(function () { toMarkdown(null); }, /null is not a string/, 'null input');
-  throws(function () { toMarkdown(void(0)); }, /undefined is not a string/, 'undefined input');
+test('invalid input', function() {
+  throws(function() {
+    toMarkdown(null);
+  }, /null is not a string/, 'null input');
+  throws(function() {
+    toMarkdown(void(0));
+  }, /undefined is not a string/, 'undefined input');
 
-  throws(function () { toMarkdown(null); }, function (e) {
+  throws(function() {
+    toMarkdown(null);
+  }, function(e) {
     return e.name === 'TypeError';
   }, 'error type');
 });
 
-asyncTest('img[onerror]', 1, function () {
+asyncTest('img[onerror]', 1, function() {
   start();
   equal(toMarkdown('>\'>"><img src=x onerror="(function () { ok(true); })()">'), '>\'>">![](x)', 'We expect img[onerror] functions not to run');
 });
